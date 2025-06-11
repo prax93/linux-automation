4392a40cde4168cffedeea38214a9e3b02ad58c8
prax93 <shana.sri@gmail.com>
2025-06-11 16:03:11 +0200

    Add Automation for Workout log

---
+++ automation/client/noter.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+STARTDATE=$(date +"%d-%m-%y")
+ENDDATE=$(date -v +7d "+%d-%m-%y")
+
+HEADER="
+## $STARTDATE - $ENDDATE"
+TEMPLATE=$(cat template.md)
+
+echo $HEADER >> ~/workoutlog.md
+echo "
+$TEMPLATE
+" >> ~/workoutlog.md 
+
+
+
+
+
