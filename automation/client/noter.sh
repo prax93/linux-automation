96e2bc5b7bfa967e6860ef98367968fe4250ac24
prax93 <shana.sri@gmail.com>
2025-06-17 15:25:08 +0200

    remove gitconfig

--- automation/client/noter.sh
+++ automation/client/noter.sh
@@ -5,7 +5,7 @@ ENDDATE=$(date -v +7d "+%d-%m-%y")
 
 HEADER="
 ## $STARTDATE - $ENDDATE"
-TEMPLATE=$(cat template.md)
+TEMPLATE=$(cat ./template.md)
 
 echo $HEADER >> ~/workoutlog.md
 echo "
--- gitconfig/README.md
+++
@@ -1,7 +0,0 @@
-# Create SSH Keys
-```bash
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/github-prax
-```
-
-# Create ssh-config file
-[Create config in ~/.ssh/config](./config)
