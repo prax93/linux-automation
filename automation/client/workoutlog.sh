d8496756ede680b139d3eafac80292d94d699287
prax93 <shana.sri@gmail.com>
2025-07-13 16:48:48 +0200

    Remove old noter.sh and template.md; add new workoutlog.sh script for workout logging

---
+++ automation/client/workoutlog.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+STARTDATE=$(date +"%d-%m-%y")
+ENDDATE=$(date -v +7d "+%d-%m-%y")
+
+HEADER="
+## $STARTDATE - $ENDDATE"
+TEMPLATE="
+| **Exercise**              | **Set 1** | **Set 2** | **Set 3** | **Set 4** | **SET 5** | **Set6** | **Set 7** | **Set 8** | **Set 9** | **Set 10** | **Rest Period** |
+| ------------------------- | ----------| --------- | --------- | --------- | --------- | -------- | --------- | --------- | --------- | ---------- | --------------- |
+| **Pull-Ups**              |           |           |           |           |           |          |           |           |           |            |                 |
+| **Negative Pull-Ups**     |           |           |           |           |           |          |           |           |           |            |                 |
+| **Shoulder Grip Pullups** |           |           |           |           |           |          |           |           |           |            |                 |
+| **Chinups**               |           |           |           |           |           |          |           |           |           |            |                 |
+| **Rows**                  |           |           |           |           |           |          |           |           |           |            |                 |
+| **Dips**                  |           |           |           |           |           |          |           |           |           |            |                 |
+| **Push-Ups**              |           |           |           |           |           |          |           |           |           |            |                 |
+| **Decline Pushups**       |           |           |           |           |           |          |           |           |           |            |                 |
+| **Arnold Dips**           |           |           |           |           |           |          |           |           |           |            |                 |
+| **Pike Push-Ups**         |           |           |           |           |           |          |           |           |           |            |                 |
+| **Pike Holds**            |           |           |           |           |           |          |           |           |           |            |                 |
+| **Curls**                 |           |           |           |           |           |          |           |           |           |            |                 |
+| **Planks**                |           |           |           |           |           |          |           |           |           |            |                 |
+| **Arnold Dips**           |           |           |           |           |           |          |           |           |           |            |                 |
+| **Planche holds**         |           |           |           |           |           |          |           |           |           |            |                 |
+"
+
+OUTPUTFILE="$HOME/workoutlog.md"
+
+if [[ $1 ]]; then
+    OUTPUTFILE=$1
+fi
+
+echo $HEADER >> "$OUTPUTFILE"
+echo "
+$TEMPLATE
+" >> "$OUTPUTFILE"
+
+
+
+
+
