75ccf98981249d537ca6a09ad66915ed44b2e8eb
prax93 <shana.sri@gmail.com>
2025-07-24 15:14:33 +0200

    Added Conditional Check whether atleast one Container is running

--- automation/homeserver/docker-retriever.sh
+++ automation/homeserver/docker-retriever.sh
@@ -1,11 +1,23 @@
+echo Starting Docker Retriever Script
 mkdir $HOME/docker-retriever
 cd $HOME/docker-retriever
+
+count="$(docker ps -q | wc -l )"
+
+if [[ $count -eq 0 ]]; then 
+    echo "No Container are running";
+
+else 
 for container in $(docker ps --format '{{.Names}}');
 do 
     mkdir "$container"
     cd "$container"
+    echo "gathering $container binds"
     docker inspect $container --format  '{{json .HostConfig.Binds}}' | jq > "$container"_binds.json
+    echo "gathering $container mounts"
     docker inspect $container --format  '{{json .Mounts}}' | jq > "$container"_mounts.json
+    echo "gathering $container ports"
     docker inspect $container --format  '{{json .NetworkSettings.Ports}}' | jq > "$container"_ports.json
 done;    
 
+fi;
