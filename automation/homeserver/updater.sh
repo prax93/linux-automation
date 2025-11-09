f2bebe2c83c09fbcda50695a0c5c6f6a885a9924
prax93 <shana.sri@gmail.com>
2025-11-09 14:34:31 +0100

    fix template

--- automation/homeserver/updater.sh
+++ automation/homeserver/updater.sh
@@ -14,7 +14,8 @@ if [ $updateExitCode -eq 0 ]; then
 	packagesLineCount=$(apt list --upgradable | wc -l)	
 
 	if [ $packagesLineCount -gt 1 ]; then
-	curl -X POST -d "$(hostname)\n $(apt list --upgradable | awk '{{ print $1 " " $2 }}')" $ntfyUrl 
+	curl -X POST -d "$(hostname)
+$(apt list --upgradable | awk '{{ print $1 " " $2 }}')" $ntfyUrl 
 	fi
 fi
 
